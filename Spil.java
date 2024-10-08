6f59ccfec4dfaab53908d5dae2f4241a257a8481
lozley <mohammad2003dk@gmail.com>
2024-10-08 17:50:43 +0200

    Update Spil.java

--- Spil.java
+++ Spil.java
@@ -5,47 +5,66 @@ class Spil {
     public static void main(String[] args) {
         var scanner = new Scanner(System.in);
         scanner.useLocale(java.util.Locale.ENGLISH);
-
-        // læser fra consol spillerens navn.
+        
+        //læser fra konsol spillerens navn
         Player player1 = new Player();
         System.out.print("Enter your name ");
         player1.name = scanner.nextLine();
 
-        // læser fra consol spillerens navn.
+
+        //læser fra konsol spillerenes navn
         Player player2 = new Player();
         System.out.print("Enter your name ");
         player2.name = scanner.nextLine();
 
 
+
         System.out.println("Hello " + player1 + " and " + player2 + ". Enjoy your game!");
 
+
+    
 
 
         //kode for at spillerne skifter tur
         boolean game = true;
         int targetpoints = 40; 
+        
 
         while (game) {
-            for (Player Current : new Player[]{player1, player2}) { 
+            for (Player Current : new Player[]{player1, player2}) {
             System.out.println(Current.name + "'s turn. press enter to roll the dice");
             scanner.nextLine();
-            
+
+
+
+             //her lægges spillerens nuværende point sammen med de point han får i den specifikke runde spilleren er nået til.
             int[] rolls = Terninger.roll();
+            if (rolls[0] == 1 && rolls[1] == 1) {
+                Current.points = 0;
+
+            } else {
             int roundPoints = rolls[0] + rolls[1];
             Current.points += roundPoints;
             System.out.println(Current.name + " rolled: " + rolls[0] + " and " + rolls[1] + ". Total points: " + Current.points);
+            }
 
 
-            if (Current.points >= targetpoints) {
+            if (Current.points >= targetpoints && rolls[0] == rolls[1]) {
                 System.out.println(Current.name + " wins with " + Current.points + " points!");
                 game = false; 
                 break; 
-
-                }
             }
+            }
+
+
+
         } 
     }
 }
+
+
+
+
 
 
 class Player {
@@ -55,7 +74,12 @@ class Player {
         public String toString() {
             return name;
         }
+        int resetPoint() {
+            return 0;
+        }
     }
+
+   
 
 
 
